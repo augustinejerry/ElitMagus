883deb46cb02c909950784c27d77e3721ee9adaa
augustinejerry <augustinejerry999@gmail.com>
2020-10-25 17:46:02 -0400

    Modularised the project and completed login backend

--- elitmagus/static/site/js/login.js
+++ elitmagus/static/site/js/login.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {                        
                 $('#login').click(function(event) {  
                     var username=$('#username').val();
-					var password=$('#password').val();
+                    var password=$('#password').val();
+                    var csrfToken=$('#token').val();
                     $.ajax( 
                         { 
                             type:"POST", 
                             url: "validatelogin", 
-                            data:{username:username, password:password}, 
+                            headers: {'X-CSRFToken': csrfToken},
+                            data: {username:username, password:password},
                             dataType: "text",
                             success: function(data) { 
                                 var obj = JSON.parse(data);
-                                alert("Hello! I am an alert box!!"+ obj.valid);
-                                // alert("Hello! I am an alert box!!" + obj.count);
                                 if (obj.valid == 1){   
                                     window.location.replace('homepage');
                                 }
                                 else{   
-                                    $('#error').text(data);
+                                    $('#error').text(obj.message);
                                 }                          
 				            },
                             error: function(data) {
